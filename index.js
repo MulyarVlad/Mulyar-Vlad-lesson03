b134ea4a3ad42a5b91eff72ae88dbd1985877a3c
Mulyar Vlad <mulyarvlad@MulyarVlad.local>
2023-02-06 11:38:31 +0200

    second commit

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ let userSecondName = prompt("Enter your Second Name");
 let userAge = +prompt("Enter your age");
 
 console.log('Your name: ' + userName + ', your second name: ' + userSecondName + ', your Age: ' + userAge + '');
+
+// 2 part
+
+let number = '12345';
+
+let arr = number.split('').join(' ');
+
+console.log(arr)
